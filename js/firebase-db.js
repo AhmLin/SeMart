1f260aefa60794d2d86ee33764bf3b28674c38aa
AhmLin <ahmadlinh01@gmail.com>
2025-10-22 15:19:37 +0700

    Update firebase-db.js

--- js/firebase-db.js
+++ js/firebase-db.js
@@ -16,12 +16,12 @@ import {
 
 // 🔥 Konfigurasi Firebase - GANTI DENGAN KONFIGURASI ANDA
 const firebaseConfig = {
-    apiKey: "your-api-key-here",
-    authDomain: "your-project.firebaseapp.com",
-    projectId: "your-project-id",
-    storageBucket: "your-project.appspot.com",
-    messagingSenderId: "123456789",
-    appId: "your-app-id"
+  apiKey: "AIzaSyApFkWDpEodKPHLzePFe0cc9z5kiMZbrS4",
+  authDomain: "semart-5da85.firebaseapp.com",
+  projectId: "semart-5da85",
+  storageBucket: "semart-5da85.firebasestorage.app",
+  messagingSenderId: "77585287575",
+  appId: "1:77585287575:web:5f58edd85981264da25cd2"
 };
 
 // Initialize Firebase
